2328e3f8940523c76e05d82ce16063395cad0feb
luciomelo1407 <luciomelo90@gmail.com>
2026-01-24 13:58:25 -0300

    se pa q foi3

--- Lucio/AnalisadorLexico/src/main/java/cmu/Main.java
+++ Lucio/AnalisadorLexico/src/main/java/cmu/Main.java
@@ -1,56 +1,55 @@
+package cmu;
 
-import cordelmaisum.lexer.Lexer;
-import cordelmaisum.parser.Parser;
-import cordelmaisum.node.*;
-import cordelmaisum.analysis.DepthFirstAdapter;
+import cmu.lexer.Lexer;
+import cmu.parser.Parser;
+import cmu.node.*;
+import cmu.analysis.DepthFirstAdapter;
 import java.io.FileReader;
 import java.io.PushbackReader;
+import java.io.File;
 
 public class Main {
 
     public static void main(String[] args) {
-        // Testando com o arquivo do triângulo (verifique se o nome está correto)
-        String arquivo = "triangulo_existencia.cmu";
+        String arquivo = "teste_simples.cmu";
 
-        System.out.println(">>> Iniciando Análise do arquivo: " + arquivo);
+        System.out.println("Working Directory = " + System.getProperty("user.dir"));
+        File f = new File(arquivo);
+        if (!f.exists()) {
+            System.err.println("Arquivo nao encontrado: " + f.getAbsolutePath());
+            return;
+        }
+
+        System.out.println(">>> Iniciando Analise do arquivo: " + arquivo);
 
         try {
-            // 1. Cria o Lexer
             Lexer lexer = new Lexer(new PushbackReader(new FileReader(arquivo), 1024));
-
-            // 2. Cria o Parser
             Parser parser = new Parser(lexer);
-
-            // 3. Gera a Árvore Sintática (AST)
             Start tree = parser.parse();
 
-            System.out.println("\n=== SUCESSO: O código compilou! ===");
-            System.out.println("Imprimindo Árvore Sintática...\n");
+            System.out.println("\n=== SUCESSO: O codigo compilou! ===");
+            System.out.println("Imprimindo Arvore Sintatica...\n");
 
-            // 4. Imprime a árvore formatada
             tree.apply(new ImpressoraArvore());
 
         } catch (Exception e) {
-            System.err.println("=== ERRO: Falha na análise ===");
+            System.err.println("=== ERRO: Falha na analise ===");
             e.printStackTrace();
         }
     }
 
-    // Classe auxiliar para imprimir a árvore com recuos (identação)
     static class ImpressoraArvore extends DepthFirstAdapter {
         private int nivel = 0;
 
         private void print(String texto) {
-            // Cria o recuo baseado no nível da árvore
-            for (int i = 0; i < nivel; i++) System.out.print("  |");
+            for (int i = 0; i < nivel; i++)
+                System.out.print("  |");
             System.out.println("- " + texto);
         }
 
         @Override
         public void defaultIn(Node node) {
-            // Quando entra num nó, imprime o nome dele e aumenta o nível
             String nomeNo = node.getClass().getSimpleName();
-            // Remove os prefixos 'A', 'P', 'T' do SableCC para ficar limpo
             nomeNo = nomeNo.replaceFirst("^(A|P|T)", "");
             print(nomeNo);
             nivel++;
@@ -58,16 +57,13 @@ public class Main {
 
         @Override
         public void defaultOut(Node node) {
-            // Quando sai do nó, diminui o nível
             nivel--;
         }
 
         @Override
         public void defaultCase(Node node) {
-            // Se for um token final (folha), imprime o valor
             if (node instanceof Token) {
                 String valor = ((Token) node).getText();
-                // Limpa quebras de linha para não bagunçar o console
                 valor = valor.replace("\n", "\\n").replace("\r", "");
                 print("'" + valor + "'");
             }
